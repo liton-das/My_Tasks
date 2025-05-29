d65466581a92344aaa9e4f43210082021e47d3ca
liton-das <litonchandra094@gmail.com>
2025-05-29 19:37:48 +0300

    Task One submited

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Task one Example Start
+let show_text=document.querySelector('.show_text');
+let change_color_btn=document.querySelector('.change_color')
+
+
+change_color_btn.addEventListener('click',()=>{
+    show_text.style='color:green'
+})
+
+// Task one Example end
